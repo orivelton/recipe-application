99369a5f2e58b2603f0f2ace7c65cb24d4224030
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-03-08 14:34:28 -0300

    chore:

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # recipe-application
 recipe-application
+
+- http://cors-anywhere.herokuapp.com/
+- https://www.food2fork.com/about/api
--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 
 import Form from  './components/Form';
 
+const API_KEY = '9776715f223be5179b2bd752a9b4f431';
+
+
 class App extends Component {
+
+  state = {
+    recipes: []
+  }
   
-  getRecipe = (e) => {
-    const recipeName = e.target.elements.recipeName.value;
-    e.preventDefault(); 
-    console.log(recipeName);
+  getRecipe = async (e) => {
+    //const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call= await fetch(`http://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
+    const data = await api_call.json();
+    
+    this.setState({
+      recipes: data.recipes
+    });
   }
 
   render() {
@@ -19,7 +31,15 @@ class App extends Component {
             Recipe Search
           </h1>
         </header>
-        <Form getRecipe={this.getRecipe}/>
+        <Form getRecipe={this.getRecipe} />
+
+        {
+          this.state.recipes.map((recipe) => {
+            return (
+              <p key={ recipe.recipe_id }>{ recipe.title }</p>
+            )
+          })
+        }
       </div>
     );
   }
